25ca65b49b8c833ad0024ec719f3955de1165151
Tej Bahadur Shrestha <venture.tej@gmail.com>
2023-05-03 18:11:11 +0545

    todo item component

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -2,6 +2,9 @@
 import { uid } from "uid";
 import { ref } from "vue";
 import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
+import {Icon} from "@iconify/vue"
+
 const todoList = ref([]);
 const createTodo = (todo) => {
   todoList.value.push({
@@ -17,11 +20,18 @@ const createTodo = (todo) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22"/>
+      <span>You have no todos to complete. Add one!</span>
+    </p>
   </main>
 </template>
 
 <style lang="scss" scoped>
-  main {
+main {
   display: flex;
   flex-direction: column;
   max-width: 500px;
@@ -33,4 +43,24 @@ const createTodo = (todo) => {
     text-align: center;
   }
 }
+
+h1 {
+    margin-bottom: 16px;
+    text-align: center;
+}
+
+.todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
 </style>
