c358769de60bf22f3dd1b4baa0eac7edf60d3f84
Tej Bahadur Shrestha <venture.tej@gmail.com>
2023-05-20 12:50:09 +0545

    edited about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="about">
-    <h1>About Vue Todo:</h1>
-    <p>Vue todo is a CRUD functionality web-app that enables the user to create a to-do list that can be executed in the future.</p>
-    <hr>
-    <h1>What I have used:</h1>
-    <p>1.Vue 3</p>
-    <p>2.localstorage to store data</p>
-    <p>3.View Router</p>
-    <p>4.SinglePageView</p>
-    <p>5.Manual CSS</p>
-    <p>6.ALl CRUD Functionality</p>
-    <p>6.Lazy Load</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+          <h1>About Vue Todo:</h1>
+          <p>Vue todo is a CRUD functionality web-app that enables the user to create a to-do list that can be executed in the future.</p>
+          <hr>
+          <h1>What I have used:</h1>
+          <p>1.Vue 3</p>
+          <p>2.localstorage to store data</p>
+          <p>3.View Router</p>
+          <p>4.SinglePageView</p>
+          <p>5.Manual CSS</p>
+          <p>6.ALl CRUD Functionality</p>
+          <p>6.Lazy Load</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: center;
   }
-}
+} */
 </style>
